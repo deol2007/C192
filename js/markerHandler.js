dfe05bceca0170cbac84ef5c9ee217ebc6b9fff3
deol2007 <73150187+deol2007@users.noreply.github.com>
2023-12-29 20:18:29 -0800

    Add files via upload

---
+++ js/markerHandler.js
@@ -0,0 +1,35 @@
+var piecesArray = [];
+
+AFRAME.registerComponent("markerhandler", {
+  init: async function () {
+    var works = await this.getPieces();
+
+    this.el.addEventListener("markerFound", () => {
+      var pieceName = this.el.getAttribute("piece_name");
+      var barcodeValue = this.el.getAttribute("barcode_value");
+      piecesArray.push({
+        piece_name: pieceName,
+        barcode_value: barcodeValue,
+      });
+
+      var work = document.querySelector(`#${pieceName}-${barcodeValue}`);
+      work.setAttribute("visible", true);
+    });
+
+    this.el.addEventListener("markerLost", () => {
+      var pieceName = this.el.getAttribute("piece_name");
+      var index = piecesArray.findIndex(
+        (x) => x.piece_name === pieceName
+      );
+      if (index > -1) {
+        piecesArray.splice(index, 1);
+      }
+    });
+  },
+
+  getPieces: function () {
+    return fetch("js/collectionList.json")
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+});
